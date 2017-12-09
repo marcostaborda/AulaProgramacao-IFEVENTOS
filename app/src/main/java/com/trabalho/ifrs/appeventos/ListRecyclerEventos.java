449e08e78cd340bc5ffb93c613084c5bca0b3152
Marcos Taborda <marcos.tabordamail@gmail.com>
2017-12-09 19:38:36 -0200

    Add Recycler Eventos

---
+++ app/src/main/java/com/trabalho/ifrs/appeventos/ListRecyclerEventos.java
@@ -0,0 +1,13 @@
+package com.trabalho.ifrs.appeventos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListRecyclerEventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_recycler_eventos);
+    }
+}
