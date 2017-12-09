a8f3a6a1cdf819a14958ba90a4d045db05dbb477
Marcos Taborda <marcos.tabordamail@gmail.com>
2017-12-09 21:54:42 -0200

    Alteração da Classe Event

--- app/src/main/java/com/trabalho/ifrs/appeventos/Event.java
+++ app/src/main/java/com/trabalho/ifrs/appeventos/Event.java
@@ -1,12 +1,15 @@
 package com.trabalho.ifrs.appeventos;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by marcos on 20/11/2017.
  */
 
 public class Event implements Serializable {
+    private int id;
     private String name;
     private int img;
     private String url;
@@ -15,6 +18,13 @@ public class Event implements Serializable {
             new Event("Mostra POA", R.drawable.mostrapoa, "http://mostra.poa.ifrs.edu.br/2017/index.html"),
             new Event("ChimaCode", R.drawable.chimacode, "http://chimacode.xyz/")
     };
+
+    public static final List<Event> eventos() {
+            List<Event> e = new ArrayList<Event>();
+            e.add(new Event("Mostra POA", R.drawable.mostrapoa, "http://mostra.poa.ifrs.edu.br/2017/index.html"));
+            e.add(new Event("ChimaCode", R.drawable.chimacode, "http://chimacode.xyz/"));
+            return e;
+    }
 
     public Event(String name, int img, String url){
         this.setName(name);
@@ -49,5 +59,13 @@ public class Event implements Serializable {
     @Override
     public String toString() {
         return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
